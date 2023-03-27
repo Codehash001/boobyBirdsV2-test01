2166cdf2a9e8059f67562f6f30d172180f8884ff
Codehash001 <codehashblockchain@gmail.com>
2023-03-28 03:04:49 +0530

    bug fixing

--- src/pages/Mint.jsx
+++ src/pages/Mint.jsx
@@ -159,14 +159,14 @@ const incrementMintAmount = () => {
               <div className='mintSection'>
                 <h5> <span>Minting is Live!</span></h5>
                 <h5 className='mintState'>{isOGstate && isValidOGUser && numberMinted < config.MAX_MINT_OG ? 'OG Mint' : isWLState && isValidWLUser && numberMinted < config.MAX_MINT_WHITELIST ? 'WhiteListed Sale' : isPublicState ?'PublicSale': ''}</h5>
-                <h6> {totalMinted} / 3100 </h6>
+                <h6 className='totalMinted'> {totalMinted} / 3100 </h6>
                 {/* + and - buttons */}
                 <div className="incButtonContainer"> 
                 <svg onClick={decrementMintAmount} xmlns="http://www.w3.org/2000/svg" className="SVG" viewBox="0 0 24 24" fill="000"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm5 11H7v-2h10v2z"></path></svg>
                 <h5 className="mintAmount">{mintAmount}</h5>
                 <svg onClick={incrementMintAmount} xmlns="http://www.w3.org/2000/svg" className="SVG" viewBox="0 0 24 24" fill="000"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"></path></svg>
                 </div>
-                <h6>Max Mint Amount: {maxMintAmount}</h6>
+                <h6 className='maxMint'>Max Mint Amount: {maxMintAmount}</h6>
                 <div className="costDiv">
                   <h6>
                     <span>Cost = {Number.parseFloat(cost * mintAmount).toFixed(4)} ETH + Gas</span>
